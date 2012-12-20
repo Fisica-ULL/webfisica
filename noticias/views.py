2d86cf19203b4238d31aab3c5906c99bfe2a02ef
Roberto Díaz Pérez <r.r.1994a@gmail.com>
2012-12-20 16:41:01 +0000

    Revert "Prueba"
    This reverts commit 48db580c53d1df03e4ecfdaf515c090cf94e8d0c.

--- noticias/views.py
+++ noticias/views.py
@@ -4,28 +4,28 @@ from django.http import HttpResponse, HttpResponseRedirect
 from noticias.models import Noticia, Comentario
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from noticias.forms import ComentarioForm, NoticiaForm
+from noticias.forms import ComentarioForm
 
 def lista_noticias(request):
     noticias = Noticia.objects.all()
     return render_to_response('lista_noticias.html',{'lista':noticias})
-
+    
 def noticia_completa(request, id_noticia):
     dato = get_object_or_404(Noticia, pk=id_noticia)
     comentarios = Comentario.objects.filter(noticia=dato)
     return render_to_response('noticia.html', {'noticia':dato, 'comentarios':comentarios}, context_instance=RequestContext(request))
-
+    
 def nueva_noticia(request):
     if request.method == 'POST':
-        formulario = NoticiaForm(request.POST, request.FILES)
+        formulario = RecetaForm(request.POST, request.FILES)
         if formulario.is_valid():
             formulario.save()
             return HttpResponseRedirect('/')
     else:
         formulario = NoticiaForm()
-
+    
     return render_to_response('noticiaform.html', {'formulario':formulario}, context_instance=RequestContext(request))
-
+    
 def nuevo_comentario(request):
     if request.method == 'POST':
         formulario = ComentarioForm(request.POST, request.FILES)
@@ -34,5 +34,5 @@ def nuevo_comentario(request):
             return HttpResponseRedirect('/')
     else:
         formulario = ComentarioForm()
-
+    
     return render_to_response('comentarioform.html', {'formulario':formulario}, context_instance=RequestContext(request))
